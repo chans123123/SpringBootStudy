de3acaac154f687ef47884b050a3bebb66e34791
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-07-29 13:07:45 +0900

    Add files via upload

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+//뷰 페이지 만들기
+@Controller //컨트롤러 선언
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) { //컨트롤러에 모델 추가하기
+        model.addAttribute("username", "홍팍"); //모델에서 변수 추가하기
+        return "greetings"; //컨트롤러에서 greetings.mustache 받아오기
+    }// end niceToMeetYou
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍팍");
+        return "goodbye";
+    }
+}
