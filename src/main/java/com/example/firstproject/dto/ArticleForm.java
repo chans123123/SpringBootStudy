62e087b39255d004bd4b09261f10a9ff921b7fa7
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-07-30 15:44:13 +0900

    Update ArticleForm.java

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -5,3 +5,15 @@ import com.example.firstproject.entity.Article;
 public class ArticleForm {
     private String title;
     private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }//structure end
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" + "title='" + title +
+                '\'' + ", content='" + content +
+                '\'' + '}';
+    }//toString end
