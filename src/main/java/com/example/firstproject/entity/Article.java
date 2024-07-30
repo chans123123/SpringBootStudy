b0e3506574626008ac1144016de8e72b4d0c9f44
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-07-30 15:45:21 +0900

    Update Article.java

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,3 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long Id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.Id = id;
+    }
