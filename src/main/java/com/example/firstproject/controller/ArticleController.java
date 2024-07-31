d1129c6853d5e285b76a839f9d75d671d00bc2f7
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-07-31 16:10:34 +0900

    Create ArticleController.java

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
