e9b2135025c93e9e76eb252af2bff1346922b1ba
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-08-01 20:32:00 +0900

    Create MemberController.java

---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
