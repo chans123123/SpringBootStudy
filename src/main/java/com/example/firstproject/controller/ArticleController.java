d28f5b4540015082ffcc996fdc09d2603c7802ea
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-08-04 11:27:26 +0900

    Update ArticleController.java

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -55,5 +55,10 @@ public class ArticleController {
     public String index(Model model) {
         // 1. 모든 데이터 가져오기
         List<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
+    }
 }
     
