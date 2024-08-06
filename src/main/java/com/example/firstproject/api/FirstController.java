2d355f43433932e853cabd4d59b912c05afc42b6
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-08-06 23:15:34 +0900

    Update FirstController.java

--- src/main/java/com/example/firstproject/api/FirstController.java
+++ src/main/java/com/example/firstproject/api/FirstController.java
@@ -3,8 +3,6 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +12,6 @@ import java.util.List;
 
 @RestController
 public class FirstApiController {
-    private static final Logger log = LoggerFactory.getLogger(FirstApiController.class);
     @Autowired
     private ArticleRepository articleRepository;
     @GetMapping("/api/articles")
