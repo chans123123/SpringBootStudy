243f5b2a1d48259381d6619a112735e1650ca53b
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-08-06 22:46:10 +0900

    Update FirstController.java

--- src/main/java/com/example/firstproject/api/FirstController.java
+++ src/main/java/com/example/firstproject/api/FirstController.java
@@ -21,3 +21,7 @@ public class FirstApiController {
     public List<Article> index() {
         return articleRepository.findAll();
     }//index end
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }// show end
