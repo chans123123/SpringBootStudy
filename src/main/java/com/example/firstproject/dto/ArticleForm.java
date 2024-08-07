e229b5cbbdd7c57171cf1986ea0e96c0cac205c9
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-08-07 23:23:49 +0900

    Update ArticleForm.java

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,23 +1,17 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
+    private Long id;
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }//structure end
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" + "title='" + title +
-                '\'' + ", content='" + content +
-                '\'' + '}';
-    }//toString end
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }// class end
